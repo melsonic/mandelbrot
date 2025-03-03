2105e9bc7ee0635694278d9a35af90de97178417
melsonic <madhurjyakalita2017@gmail.com>
2025-03-03 23:55:49 +0530

    Drawing statid mandelbrot done

--- main.go
+++ main.go
@@ -1,19 +1,43 @@
 package main
 
 import rl "github.com/gen2brain/raylib-go/raylib"
+import "image/color"
+
+const (
+	HW = 800
+	Iter = 255
+)
 
 func main() {
-	rl.InitWindow(800, 450, "raylib [core] example - basic window")
+	rl.InitWindow(HW, HW, "Mandelbrot Fractal!")
 	defer rl.CloseWindow()
 
 	rl.SetTargetFPS(60)
 
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
-
 		rl.ClearBackground(rl.RayWhite)
-		rl.DrawText("Congrats! You created your first window!", 190, 200, 20, rl.LightGray)
-
+		var i int32
+		var j int32
+		for i = 0; i<=HW; i++ {
+			for j = 0; j<=HW; j++ {
+				x0 := (float32(4*i) - 2.5*float32(HW))/HW
+				y0 := (float32(4*j) - 2*float32(HW))/HW
+				var iter uint8 = 0
+				var x float32
+				var y float32
+				for iter < Iter && x*x + y*y <= 4 {
+					x, y = x0 + x*x - y*y, y0 + 2*x*y
+					iter = iter + 1
+				}
+				if iter == Iter {
+					// finished
+					rl.DrawPixel(i, j, color.RGBA{0, 0, 0, 255})
+				} else {
+					rl.DrawPixel(i, j, color.RGBA{37, 7, 89, 255-iter})
+				}
+			}
+		}
 		rl.EndDrawing()
 	}
 }
