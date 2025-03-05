22f6938d422af66f9e20846155d082fc346bc0e1
melsonic <madhurjyakalita2017@gmail.com>
2025-03-06 00:34:34 +0530

    feature: added zoom capabilities + drag the fractal

--- main.go
+++ main.go
@@ -8,14 +8,14 @@ const (
 	Iter = 255
 )
 
-func render(xc float32, yc float32) {
+func render(xc float32, yc float32, zoom float32) {
 	rl.ClearBackground(rl.RayWhite)
 	var i int32
 	var j int32
 	for i = 0; i<=HW; i++ {
 		for j = 0; j<=HW; j++ {
-			x0 := 4*(float32(i) - xc)/float32(HW)
-			y0 := 4*(float32(j) - yc)/float32(HW)
+			x0 := 4*(float32(i) - xc)/float32(zoom*HW)
+			y0 := 4*(float32(j) - yc)/float32(zoom*HW)
 			var iter uint8 = 0
 			var x float32
 			var y float32
@@ -40,13 +40,32 @@ func main() {
 
 	var x float32 = HW/2
 	var y float32 = HW/2
+	var zoom float32 = 1.0
+	var stx float32 = -1.0
+	var sty float32 = -1.0
+	// var enx float32 = -1.0
+	// var eny float32 = -1.0
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
 		if rl.IsMouseButtonPressed(rl.MouseButtonLeft) {
 			x = float32(rl.GetMouseX())
 			y = float32(rl.GetMouseY())
 		}
-		render(x, y)
+		wheelV := rl.GetMouseWheelMove()
+		if rl.IsMouseButtonPressed(rl.MouseButtonRight) {
+			stx = float32(rl.GetMouseX())
+			sty = float32(rl.GetMouseY())
+		}
+		if rl.IsMouseButtonReleased(rl.MouseButtonRight) {
+			if stx != -1 && sty != -1 {
+				x += float32(rl.GetMouseX()) - stx
+				y += float32(rl.GetMouseY()) - sty
+			}
+			stx = -1
+			sty = -1
+		}
+		zoom += wheelV
+		render(x, y, zoom)
 		rl.EndDrawing()
 	}
 }
